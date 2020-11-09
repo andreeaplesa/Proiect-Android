26dab84705a91a4c1fc898ec9459b41c1c2231b3
andreea <liviaandreeaplesa@gmail.com>
2020-11-09 12:49:36 +0200

    add LogIn&SignUp Activity

---
+++ app/src/main/java/com/example/mymovies/User.java
@@ -0,0 +1,43 @@
+package com.example.mymovies;
+
+public class User {
+    private final String email;
+    private  String password;
+    private  String firstname;
+    private  String lastname;
+
+    public User(String email, String password, String firstname, String lastname) {
+        this.email = email;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
