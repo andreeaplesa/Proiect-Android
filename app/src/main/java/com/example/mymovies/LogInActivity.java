8437b1a59278e55d2a4820ec8a81956aadcf6a67
Alex <pizzaalex99@gmail.com>
2020-11-09 16:24:11 +0200

    added functionality to LogIn & SignUp buttons

--- app/src/main/java/com/example/mymovies/LogInActivity.java
+++ app/src/main/java/com/example/mymovies/LogInActivity.java
@@ -2,14 +2,37 @@ package com.example.mymovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
+
+    private Button btnLogIn;
+    private Button btnSingUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        //getSupportActionBar().hide();
+
+        btnLogIn = findViewById(R.id.btnLogIn);
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSingUp = findViewById(R.id.btnSignUpL);
+        btnSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mymovies/SignUpActivity.java
+++ app/src/main/java/com/example/mymovies/SignUpActivity.java
@@ -2,13 +2,27 @@ package com.example.mymovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        btnSignUp = findViewById(R.id.btnSignUpS);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
