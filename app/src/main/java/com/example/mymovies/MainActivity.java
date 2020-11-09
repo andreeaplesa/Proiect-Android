1b1a427c6ccb76671737a094062927b427b8baa8
Alex <pizzaalex99@gmail.com>
2020-11-09 20:18:45 +0200

    added fragments & bottomNavigationView

--- app/src/main/java/com/example/mymovies/MainActivity.java
+++ app/src/main/java/com/example/mymovies/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.mymovies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavView = findViewById(R.id.bottomNavView);
+
+        bottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = new DiscoverFragment();
+
+                switch (item.getItemId()){
+                    case R.id.movies:
+                        fragment = new MoviesFragment();
+                        break;
+                    case R.id.discover:
+                        fragment = new DiscoverFragment();
+                        break;
+                    case R.id.profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, fragment).commit();
+
+                return true;
+            }
+        });
     }
 }
